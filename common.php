322adc77e4a5b47d80142c808af2696862691260
Ryan C. Gordon <icculus@icculus.org>
2004-06-12 11:50:44 +0000

    Debug output stuff and stub for Wiki stuff.

--- common.php
+++ common.php
@@ -1,10 +1,28 @@
 <?php
 
+$disable_debug = false;
+
 function write_error($err)
 {
     echo "<p><center><font color='#FF0000'>";
-    echo   "$err<br>";
+    echo   "$ERROR: err<br>";
     echo "</font></center>\n";
-}
+} // write_error
+
+function write_debug($dbg)
+{
+    global $disable_debug;
+    if ($disable_debug)
+        return;
+
+    echo "<p><center><font color='#00FF00'>";
+    echo   "DEBUG: $dbg<br>";
+    echo "</font></center>\n";
+} // write_debug
+
+function get_alext_wiki_url($extname)
+{
+    return("wiki/$extname");  // !!! FIXME
+} // get_alext_wiki_url
 
 ?>
