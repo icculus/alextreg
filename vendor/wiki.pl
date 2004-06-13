e87061a77c1102ec3742f30c4eda222e09a3f408
Ryan C. Gordon <icculus@icculus.org>
2004-06-13 08:24:13 +0000

    Initial add.

---
+++ vendor/wiki.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+if (not defined $ENV{'REMOTE_USER'}) {
+    print "Content-type: text/plain\n\n";
+    print "You are misconfigured. There needs to be basic auth in this dir.\n";
+    exit 1;
+}
+
+exec '../wiki/wiki.pl';
+print "Content-type: text/plain\n\n";
+print "exec failed: $!\n";
+exit 1;
+
+# end of vendor wiki.pl ...
+
