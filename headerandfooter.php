72536a6b09ceb972cc0ea262d07ddd34f9af2a3e
Ryan C. Gordon <icculus@icculus.org>
2004-06-14 17:59:52 +0000

    syntax typo.

--- headerandfooter.php
+++ headerandfooter.php
@@ -15,7 +15,7 @@ function render_footer()
 {
     // !!! FIXME: need more here, I guess.
     echo "<hr>\n";
-    if (is_authorized_user())
+    if (is_authorized_vendor())
         echo "<i>Logged in as: ${_SERVER['REMOTE_USER']}<br>\n";
     echo "</body></html>\n";
 } // render_footer
