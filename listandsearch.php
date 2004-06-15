2b03e575719fd73a89f1cdf3a82cd5ad0bde6bc4
Ryan C. Gordon <icculus@icculus.org>
2004-06-15 15:40:18 +0000

    Corrected SQL.

--- listandsearch.php
+++ listandsearch.php
@@ -129,9 +129,9 @@ function find_tokenvalue($wantname)
     $additionalsql = '';
     if ($wantname)
     {
-        if (sscanf($wantname, "0x%X", &$sqlwantname) != 1)
-            $sqlwantname = db_escape_string($wantname);
-        $additionalsql .= " where tok.tokenval=$sqlwantname";
+//        if (sscanf($wantname, "0x%X", &$sqlwantname) != 1)
+//            $sqlwantname = db_escape_string($wantname);
+        $additionalsql .= " and (tok.tokenval=$sqlwantname)";
     } // if
 
     find_token($additionalsql, $wantname);
