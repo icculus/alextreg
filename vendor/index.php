9596c8c97c567f04ede80bb5c08781ee5c21b6a0
Ryan C. Gordon <icculus@icculus.org>
2004-06-22 20:40:09 +0000

    Added some semicolons.

--- vendor/index.php
+++ vendor/index.php
@@ -243,9 +243,9 @@ function op_delext()
     {
         $htmlextname = htmlentities($extname, ENT_QUOTES);
         echo "About to delete an extension named '$htmlextname'<br>\n";
-        echo "<b><font size="+1">\n"
+        echo "<b><font size="+1">\n";
         echo "THERE IS NO UNDELETE. MAKE SURE YOU <u>REALLY</u> WANT TO DO THIS.<br>\n";
-        echo "</font></b>\n"
+        echo "</font></b>\n";
         echo "...if you're sure, click 'Confirm'...<br>\n";
         echo "<form>\n";
         echo "<input type='hidden' name='extid' value='$extid'>\n";
