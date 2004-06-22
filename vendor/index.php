68f5eb7f2fdac28c8ba5945aba935fef831051d0
Ryan C. Gordon <icculus@icculus.org>
2004-06-22 20:39:16 +0000

    Added a little emphasis.

--- vendor/index.php
+++ vendor/index.php
@@ -244,7 +244,7 @@ function op_delext()
         $htmlextname = htmlentities($extname, ENT_QUOTES);
         echo "About to delete an extension named '$htmlextname'<br>\n";
         echo "<b><font size="+1">\n"
-        echo "THERE IS NO UNDELETE. MAKE SURE YOU REALLY WANT TO DO THIS.<br>\n";
+        echo "THERE IS NO UNDELETE. MAKE SURE YOU <u>REALLY</u> WANT TO DO THIS.<br>\n";
         echo "</font></b>\n"
         echo "...if you're sure, click 'Confirm'...<br>\n";
         echo "<form>\n";
