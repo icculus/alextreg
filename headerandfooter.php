a6e53d50722f524fdbacde80ab7e114aa982aa50
Ryan C. Gordon <icculus@icculus.org>
2004-06-24 05:38:53 +0000

    Dir fix.

--- headerandfooter.php
+++ headerandfooter.php
@@ -4,6 +4,8 @@ require_once 'common.php';
 
 function render_header($title = 'OpenAL Extension Registry')
 {
+    $img = ((is_authorized_vendor()) ? '../' : '') . 'openal_title_sm.jpg';
+
 // !!! FIXME: need more here, I guess.
 echo <<< EOF
 <html><head><title>$title</title></head><body>
